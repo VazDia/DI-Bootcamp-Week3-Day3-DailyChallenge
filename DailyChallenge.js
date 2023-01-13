e5751d0b4a60549410c226600b75fb381ceaf5cf
Vazoumana Diarrassouba <121106960+VazDia@users.noreply.github.com>
2023-01-13 01:05:10 +0000

    Update DailyChallenge.js

--- DailyChallenge.js
+++ DailyChallenge.js
@@ -1,18 +1,29 @@
-let inputText = document.createElement("input");
-inputText.type = "text";
+/*
+Create an input type text that takes/shows only letters. (ie. numbers and special characters won’t be accepted)
 
-document.body.appendChild(inputText);
+Hint: use one of the following events to remove any character that is not a letter
 
-inputText.addEventListener("keyup", KeyUp);
+keyup event
+or keypress event
+or keydown event
+or input event
 
-function KeyUp() {
-  let regEx = /^[a-zA-Z]+$/.test(inputText.value);
-  if (!regEx) {
-    inputText.value = inputText.value.replace(/[0-9]/g, "");
-  } else {
-    let texte = document.createElement("p");
-    texte.innerHTML = inputText.value;
-    texte.style.display = "inline-Block";
-    document.body.appendChild(texte);
-  }
+Hint : Check out keycodes in Javascript or Regular Expressions
+*/ 
+let input = document.querySelector("#text"); // We could use "document.getElementById('text')"
+let display = document.querySelector("#display");
+
+input.addEventListener("keypress",lettersOutput);
+
+function lettersOutput(e){
+   let letters=e.target.value;
+   if (e.keyCode<65 || e.keyCode>122 || (90<e.keyCode && e.keyCode<97)){ // the input will not take care of keys out of "a-z-A-Z"
+       e.preventDefault();
+       console.log(e.keyCode)
+   }
+    else{
+       letters=letters+ e.key;
+       display.innerHTML=letters
+       display.style.fontSize="60px";
+   }
 }
